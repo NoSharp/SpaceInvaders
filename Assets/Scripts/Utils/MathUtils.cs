3f54abd095c44c2b83c72da0fa69538aae787fcf
nosharp <nosharp1@gmail.com>
2021-06-09 09:11:58 +0100

    :sparkles:: Add Clamp extension method for vectors

--- Assets/Scripts/Utils/MathUtils.cs
+++ Assets/Scripts/Utils/MathUtils.cs
@@ -2,7 +2,7 @@
 
 namespace Utils
 {
-    public class MathUtils
+    public static class MathUtils
     {
 
         public static Vector2 LerpVector(Vector2 from, Vector2 to, float t)
@@ -10,6 +10,12 @@ namespace Utils
             from.x = Mathf.Lerp(from.x, to.x, t);
             from.y = Mathf.Lerp(from.y, to.y, t);
             return from;
+        }
+
+        public static Vector2 ClampX(this Vector2 current, Vector2 mins, Vector2 maxs)
+        {
+            current.x = Mathf.Clamp(current.x, mins.x, maxs.x);
+            return current;
         }
 
     }
