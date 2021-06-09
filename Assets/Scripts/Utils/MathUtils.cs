1c718b0d0f0c1b394ad8b29d34731695d1163090
nosharp <nosharp1@gmail.com>
2021-06-09 08:46:39 +0100

    feat: add utility to LerpVector2's

---
+++ Assets/Scripts/Utils/MathUtils.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+
+namespace Utils
+{
+    public class MathUtils
+    {
+
+        public static Vector2 LerpVector(Vector2 from, Vector2 to, float t)
+        {
+            from.x = Mathf.Lerp(from.x, to.x, t);
+            from.y = Mathf.Lerp(from.y, to.y, t);
+            return from;
+        }
+
+    }
+}
