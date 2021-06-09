3a174f2b7d9e8388904cec56b09065250dfcc348
nosharp <nosharp1@gmail.com>
2021-06-09 09:12:34 +0100

    :sparkles:: Add clamping to the X position of a space invader

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -3,14 +3,17 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEditor;
 using UnityEngine;
+using Utils;
 using MathUtils = Utils.MathUtils;
 
 public class PlayerController : MonoBehaviour
 {
 
+    private readonly Vector2 _cameraMins = new Vector2(-10, -10);
+    private readonly Vector2 _cameraMaxs = new Vector2(10, 10);
+    
     private SpriteRenderer _playerRenderer;
     private Rigidbody2D _playerRigidbody;
-
     private Vector3 _wishVelocity;
     
     void Start()
@@ -29,17 +32,7 @@ public class PlayerController : MonoBehaviour
     {
         var xDir = 0;
         var yDir = 0;
-
-        if (Input.GetKey(KeyCode.W))
-        {
-            yDir += 1;
-        }
-
-        if (Input.GetKey(KeyCode.S))
-        {
-            yDir -= 1;
-        }
-
+        
         if (Input.GetKey(KeyCode.A))
         {
             xDir -= 1;
@@ -57,5 +50,6 @@ public class PlayerController : MonoBehaviour
     {
         _wishVelocity = GetDirection() * 15;
         _playerRigidbody.velocity = MathUtils.LerpVector(_playerRigidbody.velocity, _wishVelocity, Time.deltaTime*5);
+        _playerRigidbody.position = _playerRigidbody.position.ClampX(_cameraMins, _cameraMaxs);
     }
 }
